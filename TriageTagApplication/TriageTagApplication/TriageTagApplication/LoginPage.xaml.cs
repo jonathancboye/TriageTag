b2c94f38d9268da992a970a6f3798d87bfb1a76d
Jonathan Carpenter <carpenter.prime@gmail.com>
2016-11-01 05:07:24 -0400

    Created login page

---
+++ TriageTagApplication/TriageTagApplication/TriageTagApplication/LoginPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TriageTagApplication
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage() {
+            Padding = new Thickness( 5, 5, 5, 5 );
+            InitializeComponent();
+        }
+    }
+}
