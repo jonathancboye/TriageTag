36b003df7f0963dce4594ccd143970d9252d13e4
Jonathan Carpenter <carpenter.prime@gmail.com>
2016-11-10 00:57:26 -0500

    fix namespace for Editable.cs

--- TriageTagApplication/TriageTagApplication/TriageTagApplication/ViewPages/EditMedicalDataPage.xaml.cs
+++ TriageTagApplication/TriageTagApplication/TriageTagApplication/ViewPages/EditMedicalDataPage.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
-using TriageTagApplication.Model;
 using Xamarin.Forms;
 
 namespace TriageTagApplication
